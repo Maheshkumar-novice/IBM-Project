ac6612dbfdb7362755f1d42b99bd27adb06044ff
Maheshkumar <callmeelprofessor@gmail.com>
2022-11-14 23:33:30 +0530

    Fix unique name product bug fix

--- Project Development Phase/sprint 2/backend/application/product/forms.py
+++ Project Development Phase/sprint 2/backend/application/product/forms.py
@@ -9,7 +9,7 @@ class ProductForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
 
-    def validate_product_name(self, name):
+    def validate_name(self, name):
         product = Product.query.filter_by(name=name.data).first()
         if product is not None:
             raise ValidationError('Product already exists!')
