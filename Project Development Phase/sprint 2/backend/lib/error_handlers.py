26af8af919d3d5f5ae0cb864b57e44f97dd1d334
Maheshkumar <callmeelprofessor@gmail.com>
2022-11-14 21:27:44 +0530

    Delete old lib files

--- Project Development Phase/sprint 2/backend/lib/error_handlers.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app
-
-from lib.response import response
-
-
-@current_app.errorhandler(404)
-def error_handler_404(_error):
-    return response(status_code=404, status='error', data={}, message='Requested resource/page Found.')
