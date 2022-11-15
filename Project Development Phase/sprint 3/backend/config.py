76baccb12edb225f961c8d159ab04e088b18d272
Maheshkumar <callmeelprofessor@gmail.com>
2022-11-15 22:56:50 +0530

    Refactor config by alphabetical order

--- Project Development Phase/sprint 3/backend/config.py
+++ Project Development Phase/sprint 3/backend/config.py
@@ -16,16 +16,17 @@ class Config():
         'EMAIL_CONFIRMATION_SENDER_EMAIL')
     EMAIL_CONFIRMATION_SALT = 'email-confirmation'
     EMAIL_CONFIRMATION_TOKEN_MAX_AGE_SECONDS = 300
-    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(32))
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-    WTF_CSRF_ENABLED = False
-    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=60)
     JSON_SORT_KEYS = False
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=60)
+    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(32))
+    SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    JSON_SORT_KEYS = True
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "app.db")}'
 
