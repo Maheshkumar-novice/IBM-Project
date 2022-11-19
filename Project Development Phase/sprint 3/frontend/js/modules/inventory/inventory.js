6ca663bc36fa1ab5bd8e378c0fff71770f4598d2
Lakshmikant K <lakshmikant2512@gmail.com>
2022-11-19 12:44:44 +0530

    load inventory form inputs and integratre create inventory API

--- Project Development Phase/sprint 3/frontend/js/modules/inventory/inventory.js
+++ Project Development Phase/sprint 3/frontend/js/modules/inventory/inventory.js
@@ -1,8 +1,14 @@
 import { constants } from "../../constants.js";
+import { alert } from "../../utils.js";
+
 
 const loader = document.querySelector("#loader");
 const inventoriesWrapper = document.querySelector("#inventories");
-
+const formToggleBtn = document.querySelector("#form-toggle-btn");
+const addInventoryForm = document.querySelector("#add-inventory-form");
+const messageBox = document.querySelector(".message-box");
+const productNameInput = document.querySelector('#product-name');
+const locationNameInput = document.querySelector('#location-name');
 const token = localStorage.getItem("token");
 const requestHeaders = {
   headers: {
@@ -12,7 +18,25 @@ const requestHeaders = {
 
 window.addEventListener('load', () => {
   loader.classList.remove('d-none');
+  getProducts();
+  getLocations();
   getInventory();
+});
+
+formToggleBtn.addEventListener("click", () => {
+  addInventoryForm.classList.toggle("d-none");
+});
+
+addInventoryForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loader.classList.remove("d-none");
+  const api_body = {
+    product_name: e.target[0].value,
+    location_name: e.target[1].value,
+    quantity: e.target[2].value,
+    threshold: e.target[3].value
+  }
+  addInventory(api_body);
 });
 
 const getInventory = async () => {
@@ -28,6 +52,52 @@ const getInventory = async () => {
   }
   finally {
     loader.classList.add('d-none');
+  }
+}
+
+const addInventory = async (api_body) => {
+  try {
+    const { data } = await axios.post(
+      `${constants.BASE_URL}/inventory/`,
+      api_body, requestHeaders);
+    addInventoryForm.classList.add('d-none');
+    alert(messageBox, data.message, 'success');
+    getInventory();
+    resetInventoryForm();
+  }
+  catch (error) {
+    console.error("Add Product Error: ", error);
+    alert(messageBox, error.response.data.message, 'failed');
+  }
+  finally {
+    loader.classList.add("d-none");
+  }
+}
+
+const getProducts = async () => {
+  try {
+    const { data } = await axios.get(
+      `${constants.BASE_URL}/products/all`,
+      requestHeaders
+    );
+    createFormOptions(productNameInput,data.data)
+  }
+  catch (error) {
+    console.error("Get Products Error: ", error);
+  }
+}
+
+
+const getLocations = async () => {
+  try {
+    const { data } = await axios.get(
+      `${constants.BASE_URL}/locations/all`,
+      requestHeaders
+    );
+    createFormOptions(locationNameInput,data.data)
+  }
+  catch (error) {
+    console.error("Get Products Error: ", error);
   }
 }
 
@@ -55,4 +125,18 @@ const createInventoryCards = (inventories) => {
         <p class="item-des inventory-threshold pt-0">Threshold: <span>${inventory.threshold}</span></p>
       </div>`;
   });
+}
+
+const createFormOptions = (element, data) => {
+  data.map((d) => {
+    element.innerHTML +=
+      `<option value=${d.name}>${d.name}</option>`
+  })
+}
+const resetInventoryForm = () => {
+  const formInputs = document.querySelectorAll('.app-input');
+  formInputs.forEach((input) => {
+    console.log(input);
+    input.value = '';
+  })
 }
